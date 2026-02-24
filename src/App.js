cfb7d28fcb922a13faf5e44fe4983479baa62131
Gurneet <gsingh2@atb.com>
2026-02-23 23:48:38 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="logo">
+          <h2>Harmeet</h2>
+          <h6>--Training Gym--</h6>
+        </div>
+        <div className="nav">
+          <ul>
+            <li>Home</li>
+            <li>Memberships</li>
+            <li>Facilities</li>
+            <li>Classes</li>
+            <li>Shop</li>
+          </ul>
+        </div>
+        <button className="login-btn">Join Now</button>
+      </div>
+      <div className="hero">
+        <video className="hero-video" autoPlay muted loop>
+          <source
+            src="/assets/456653_Gym Training Workout Exercise_By_Cinematic_Vision_Artlist_HD.mp4"
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
+        </video>
+      </div>
     </div>
   );
 }
